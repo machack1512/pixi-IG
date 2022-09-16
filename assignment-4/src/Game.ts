036e262d9d9120e9ba6d36645acceb3cf68c0b7b
machack1512 <2019pcecsshahrin163@poornima.org>
2022-09-16 17:29:18 +0530

    final

--- assignment-4/src/Game.ts
+++ assignment-4/src/Game.ts
@@ -1,12 +1,15 @@
 import {Application, Sprite, Text} from 'pixi.js';
+import * as PIXI from "pixi.js";
 import {gsap} from 'gsap';
 export class Game extends Application{
     private spin:boolean;
-    private sliceAngle = 360/7;
+    private sliceAngle = 360/12;
     constructor(opts:any) {
         super(opts);
         this.preload([
-            {name:'wheel', url:'assets/wheel.png'}
+            {name:'wheel', url:'assets/spinwheel.png'},
+            {name:'pointer',url:'assets/pointer.png'},
+            { name: "bg", url: "assets/bg.jfif" },
         ], this.onLoad.bind(this));
     }
     preload(list:any[], cb:()=>{}):void {
@@ -14,17 +17,43 @@ export class Game extends Application{
         this.loader.load(cb);
     }
     onLoad():void {
+        const bg = new Sprite(this.loader.resources['bg'].texture);
+        bg.anchor.set(0.01);
+         bg.width=1200;
+        bg.height=550;
+     // bg.x = 765;
+     // bg.y = 60;
+        this.stage.addChild(bg);
         const wheel = new Sprite(this.loader.resources['wheel'].texture);
         wheel.anchor.set(0.5);
         wheel.x = this.screen.width/2;
         wheel.y = this.screen.height/2;
+        
+
         this.stage.addChild(wheel);
         wheel.interactive = true;
         wheel.buttonMode = true;
         console.log(this.stage);
+       
+
+
+
+        const pointer = new Sprite(this.loader.resources['pointer'].texture);
+        pointer.anchor.set(0.01);
+        pointer.width=50;
+        pointer.height=50;
+        pointer.x = 765;
+        pointer.y = 60;
+        this.stage.addChild(pointer);
+
+
+
         wheel.on('pointerup', ()=>{
-            let random = Math.floor(Math.random()*7);
+            let random = Math.floor(Math.random()*12);
+
+           // console.log(random);
             let stopAngle = random * this.sliceAngle;
+          //  console.log(stopAngle);
             gsap.fromTo(wheel,{angle:0},{angle:3600+stopAngle, duration:5, ease:'expo.out'});
             const myTimeout = setTimeout(myStopFunction.bind(this), 5000);
 
@@ -34,18 +63,12 @@ export class Game extends Application{
                
                var i=0;
                var ar:number[]=[1,12,11,10,9,8,7,6,5,4,3,2,1];
-               let  finalMessage = new Text("YOU WON "+ar[random]);
-                        
-                    finalMessage.x = 700;
-                     finalMessage.y = 640;
-                    this.stage.addChild(finalMessage);
+               
+               
 
-                    console.log(ar[random]);
-                    setTimeout(displa,3000);
-                    function displa():void{
-                     finalMessage.visible=false;
-                    }
 }
+
+
         });
     }
 }
